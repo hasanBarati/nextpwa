c6eda5b6b4a2c3d282f76f5c8d547cecc856b13a
hasanb69 <hasanbaratiasp@gmail.com>
2023-11-26 10:32:28 +0330

    change geo location

--- app/page.tsx
+++ app/page.tsx
@@ -16,12 +16,31 @@ export default function Home() {
   const click=async ()=>{
 
 
+    // https://www.google.com/maps/place/{lat},{lug}
+    // const url = `https://www.google.com/maps/place/${35.84647388697808},${50.95969676971436}`
+    const url = `https://nshn.ir/maps/place/${35.84647388697808},${50.95969676971436}`
+    window.open(url)
+
+    // const data  = {url: `https://www.waze.com/ul?ll=${[130,5545,57.547788]}&navigate=yes`, text: "text", title: 'title'} ;
+    // // const shareData = {
+    // //   url: item,
+    // // };
+
+    
+    // // window.open(wazeUrl, '_blank');
+
+    // try {
+    //   await navigator.share(data);
+    // }
+    // catch(e) {
+    //   console.log('share error', e);
+    // }
   }
   return(
     <>
         <h1>Hello, Next.js! changed th title again</h1>
         <button onClick={click}>click</button>
-        <a href="geo:124.028582,-29.201930" target="_blank">Click here for map</a>
+        <a href="geo:35.84647388697808,50.95969676971436" target="_blank">Click here for map</a>
     </>
 
   )
